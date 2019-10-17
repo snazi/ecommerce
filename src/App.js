9e474fd308ab563a6679385140e59af6ce0c5369
Angelo Amadora <amadora.angelo@gmail.com>
2019-10-17 16:52:06 +0800

    added homepage.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from  './components/HomePage'
 function App() {
   return (
-    <div>Hello World</div>
+    <HomePage></HomePage>
   );
 }
 
-export default App;
+export default App
