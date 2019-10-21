1fa7d564ece077a7bf47a0cb593cdba19723f7b7
Angelo <amadora.angelo@gmail.com>
2019-10-21 17:07:51 +0800

    added some string interpolation for the sizes

--- src/components/directory/index.js
+++ src/components/directory/index.js
@@ -47,8 +47,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map( ({title, imageUrl, id}) => (
-                        <MenuItem key={id} title={title} img={imageUrl}> </MenuItem>
+                    this.state.sections.map( ({title, imageUrl, id, size }) => (
+                        <MenuItem key={id} title={title} img={imageUrl} size={size}> </MenuItem>
                     ))
                 }
             </div>
--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './module.style.scss'
 
-const MenuItem = ({title}) => (
-    <div className='menu-item'>
+const MenuItem = ({ title, img, size }) => (
+    <div style={{
+        backgroundImage: `url(${img})`
+    }} className={`${size} menu-item`}>
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
