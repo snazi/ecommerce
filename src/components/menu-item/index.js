4137b5770f2a0ac0278d1de738ae2dfbfa6128ed
Angelo Amadora <amadora.angelo@gmail.com>
2019-11-07 17:13:35 +0800

    added hovering

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './module.style.scss'
 
 const MenuItem = ({ title, img, size }) => (
-    <div style={{
-        backgroundImage: `url(${img})`
-    }} className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+
+        <div className='background-image'  style={{
+            backgroundImage: `url(${img})`
+            }}/>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
