6da23224748dfb5b30bc6cd8b716c9508a4bda77
Angelo Amadora <amadora.angelo@gmail.com>
2019-11-07 17:47:01 +0800

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 
+//import for router
+import { Route } from 'react-router-dom'
+
 import HomePage from  './pages/HomePage'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
-    <HomePage></HomePage>
+    <div>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
+    </div>
   );
 }
 
