6be21a8285aec0bec0dd7227c6f0b4bbd2c42213
Angelo <amadora.angelo@gmail.com>
2019-11-13 14:43:14 +0800

    added three different pages

--- src/App.js
+++ src/App.js
@@ -6,17 +6,37 @@ import { Route } from 'react-router-dom'
 
 import HomePage from  './pages/HomePage'
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+const HatsPage = () => {
+    return (
+      <div>
+        <h1>Hats Page</h1>
+      </div>
+    )
+}
+
+const TopicsList = () => {
+  return (
+    <div>
+      <h1>Topics List</h1>
+    </div>
+  )
+}
+
+const TopicDetail = () => {
+  return (
+    <div>
+      <h1>Topics Detailz</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/hats' component={HatsPage} />
+      <Route exact path='/topics' component={TopicsList} />
+      <Route exact path='/topics/:topicId' component={TopicDetail} />
     </div>
   );
 }
