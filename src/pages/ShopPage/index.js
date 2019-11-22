263420f99892a7eab25443142d3841fac9ccc031
Angelo Amadora <amadora.angelo@gmail.com>
2019-11-22 12:52:30 +0800

    added shops component

---
+++ src/pages/ShopPage/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+
+constructor(props) {
+    super(props)
+
+    this.state = {
+         
+    }
+}
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default index
